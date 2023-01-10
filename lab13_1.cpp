4e3fa50513152fd565f62402662a99157f4ea508
ShinRunner1st <shinrunner1st@gmail.com>
2023-01-10 16:25:46 +0700

    FFF

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 using namespace std;
 
+long long int fibonacci(long long int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+long long int fibonacci(long long int x)
+{
+    if(x == 0 || x == 1) return x;
+    else return fibonacci(x-1)+fibonacci(x-2);
 }
